00991398764a86972e469a8543306af235289255
semanticist21 <92710478+semanticist21@users.noreply.github.com>
2022-03-13 23:34:11 +0900

    Add files via upload

---
+++ updater-using-sftp/Model/CustomConnectionInfo.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Updater.model
+{
+    public struct CustomConnectionInfo
+    {
+        /// <summary>
+        /// IP address used for the connection.
+        /// </summary>
+        public string Address { get; set; }
+        /// <summary>
+        /// sftp port. Usually 22.
+        /// </summary>
+        public int Port { get; set; }
+        /// <summary>
+        /// User name used for the connection.
+        /// </summary>
+        public string User { get; set; }
+        /// <summary>
+        /// password used for the connection.
+        /// </summary>
+        public string Password { get; set; }
+        /// <summary>
+        /// It represents base file directory where update files will be put into.
+        /// example : C:/dir1/dir2
+        /// </summary>
+        private string fileDirectory;
+
+        public string FileDirectory
+        {
+            get
+            {
+                return fileDirectory;
+            }
+            set
+            {
+                if (fileDirectory != value)
+                {
+                    if (!value.Contains('\\')) fileDirectory = value;
+                    else
+                    {
+                        fileDirectory = value.Replace("\\", "/").Replace("\\", "/");
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// It represents server base directory.
+        /// example : /
+        /// </summary>
+        public string SftpFileBaseDirectory { get; set; }
+    }
+}
---
+++ updater-using-sftp/Model/FileInfoData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Updater.model
+{
+    public struct FileInfoData
+    {
+        public string Name { get; set; }
+        private string directory;
+        /// <summary>
+        /// folder/folder/.../fileName, Except base directory
+        /// </summary>
+        public string Directory
+        {
+            get { return directory; }
+            set
+            {
+                if (directory != value)
+                {
+                    if (!value.First().Equals("\\")) directory = value;
+                    else directory = string.Concat(value.Skip(1));
+                }
+
+            }
+        }
+        public DateTime LastWrittenTime { get; set; }
+    }
+}
